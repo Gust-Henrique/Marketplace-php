84a2fdc25dfc93ee67eaa4a43a508b944efabff2
Cassiano-Behemonth <oliveira.cassiano@escola.pr.gov.br>
2025-06-15 02:27:28 -0300

    criação da categoria

---
+++ model/Categoria.php
@@ -0,0 +1,67 @@
+Categoria.php
+[02:26]
+<?php
+require_once '../config/database.php';
+
+class Categoria {
+    private $conn;
+    private $table = "categorias";
+
+    public $id;
+    public $nome;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function cadastrar() {
+        $query = "INSERT INTO " . $this->table . " (nome) VALUES (:nome)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':nome', $this->nome);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function listar() {
+        $query = "SELECT * FROM " . $this->table . " ORDER BY nome";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function buscarPorId($id) {
+        $query = "SELECT * FROM " . $this->table . " WHERE id = :id LIMIT 1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function atualizar() {
+        $query = "UPDATE " . $this->table . " SET nome = :nome WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':nome', $this->nome);
+        $stmt->bindParam(':id', $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function excluir($id) {
+        $query = "DELETE FROM " . $this->table . " WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+}
+?>
